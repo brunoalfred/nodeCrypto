0ef94911039fc0a5fb3f2cb656a47b21bbc3ce4b
brunoalfred <alfredybruno@gmail.com>
2021-04-04 20:02:51 +0300

    created encrypt and decrypt methods

--- encrytption.js
+++ encrytption.js
@@ -1,23 +1,18 @@
-
-try {
-    let  crypto = require('crypto');
-} catch (err) {
-    console.log('crypto support is disabled!');
-} finally {
+let  crypto = require('crypto');
 
 
+const encrypt = (value, password) => {
+    
     const algorithm = 'aes-192-cbc';
-    const password = 'Password used to generate key';
 
-    // First, we'll generate the key. The key length is dependent on the algorithm.
-    // In this case for aes192, it is 24 bytes (192 bits).
+
     crypto.scrypt(password, 'salt', 24, (err, key) => {
         if (err) throw err;
-        // Then, we'll generate a random initialization vector
+
         crypto.randomFill(new Uint8Array(16), (err, iv) => {
             if (err) throw err;
 
-            // Once we have the key and iv, we can create and use the cipher...
+
             const cipher = crypto.createCipheriv(algorithm, key, iv);
 
             let encrypted = '';
@@ -26,9 +21,13 @@ try {
             cipher.on('data', (chunk) => encrypted += chunk);
             cipher.on('end', () => console.log(encrypted));
 
-            cipher.write('some clear text data');
+            cipher.write(value);
             cipher.end();
         });
     });
+
+
+
 }
 
+module.exports = encrypt;
---
+++ index.js
@@ -0,0 +1,19 @@
+let crypto;
+try {
+    crypto = require('crypto');
+} catch (error) {
+    console.log('crypto support is disabled!');
+ } finally{
+
+
+    // Encryption method
+
+    
+
+
+    // Decryption method
+
+
+ }
+
+
