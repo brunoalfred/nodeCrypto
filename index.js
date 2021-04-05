7b2220cecf0aa8e7c56cd155be7fe5f5555c2c7f
brunoalfred <alfredybruno@gmail.com>
2021-04-05 15:54:17 +0300

    trying the utils module

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-// const encryptionMethod = require('./encryption') ;
-// const decryptionMethod = require('./decryption');
+const util = require('util')
 
-// let crypto;
-// try {
-//     crypto = require('crypto');
-// } catch (error) {
-//     console.log('crypto support is disabled!');
-//  } finally{
+async function hello(){
+    return 'Hello world';
+}
 
+const callbackFunction = util.callbackify(hello);
 
-//     // Encryption method
+callbackFunction((err, resp)=> {
+    if (err) throw err;
 
-//     const encrypted_value = encryptionMethod('helloo', 'pass');
-//     console.log(encrypted_value);
-
-
-
-
-
-//     // Decryption method
-//     const decrypted_value = decryptionMethod('65e1d8fb8601aff8160c8121a9532e6c', 'pass');
-//     console.log(decrypted_value);
-
-//  }
-
-
-
-const dgram = require('dgram');
-const message = Buffer.from('Some bytes');
-const client = dgram.createSocket('udp4');
-client.send(message, 41234, 'localhost', (err) => {
-    client.close();
-});
+    console.log(resp);
+})
